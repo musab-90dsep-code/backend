ae9e42d995e48ceeab46acfbfe4e74100c565209
musab-90dsep-code <musab-90dsep@dipti.com.bd>
2026-03-10 03:06:59 +0600

    added localhost to cors

--- madrasa_backend/settings.py
+++ madrasa_backend/settings.py
@@ -123,6 +123,13 @@ STATIC_URL = 'static/'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
 import os
 
 MEDIA_URL = '/media/'
