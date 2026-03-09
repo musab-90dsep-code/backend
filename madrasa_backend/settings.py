562ab32eef90bb0fef88dc06a7d7ef14bcfa792b
musab-90dsep-code <musab-90dsep@dipti.com.bd>
2026-03-10 03:27:56 +0600

    added localhost to cors

--- madrasa_backend/settings.py
+++ madrasa_backend/settings.py
@@ -127,7 +127,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://markaz-sepia.vercel.app/"
+    
 ]
 
 import os
