50b4260b64f7d484516136a1bd4476cf6098ec01
musab-90dsep-code <musab-90dsep@dipti.com.bd>
2026-03-11 23:52:39 +0600

    Update Cloudinary storage settings and MEDIA_URL

--- madrasa_backend/settings.py
+++ madrasa_backend/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
